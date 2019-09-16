9994deccfa5feeb335efbb26cc3278fc71c9f5c1
Magdiel Nathan <55208169+Magdielnathan@users.noreply.github.com>
2019-09-15 21:10:50 -0300

    Update App.java

--- aep/src/aepArthur/App.java
+++ aep/src/aepArthur/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Conceito c1 = new Conceito("1", 9, "prova1", "Joana");
+		Conceito c1 = new Conceito("1", new Nota(9.0), new Avaliação("prova1"), new Aluno("Joana"));
 		 
 	}
 }
